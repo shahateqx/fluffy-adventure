fb4df91105c36b400a91d91519a6dcb59dd51fb7
shahateqx <shahat0209@gmail.com>
2026-05-03 15:21:43 +0600

    inngestvariable

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -6,70 +6,43 @@ import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 export const inngest = new Inngest({ id: "talent-aq" });
 
 const syncUser = inngest.createFunction(
-  {
-    id: "sync-user",
-    triggers: [{ event: "clerk/user.created" }],
-  },
+  { id: "sync-user", trigger: { event: "clerk/user.created" } },
   async ({ event }) => {
     await connectDB();
-
     const { id, email_addresses, first_name, last_name, image_url } = event.data;
-
     const newUser = {
       clerkId: id,
       email: email_addresses[0]?.email_address,
       name: `${first_name || ""} ${last_name || ""}`,
       profileImage: image_url,
     };
-
     await User.create(newUser);
-
-    await upsertStreamUser({
-      id: newUser.clerkId.toString(),
-      name: newUser.name,
-      image: newUser.profileImage,
-    });
+    await upsertStreamUser({ id: newUser.clerkId.toString(), name: newUser.name, image: newUser.profileImage });
   }
 );
 
 const deleteUserFromDB = inngest.createFunction(
-  {
-    id: "delete-user-from-db",
-    triggers: [{ event: "clerk/user.deleted" }],
-  },
+  { id: "delete-user-from-db", trigger: { event: "clerk/user.deleted" } },
   async ({ event }) => {
     await connectDB();
-
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
-
     await deleteStreamUser(id.toString());
   }
 );
 
 const updateUserInDB = inngest.createFunction(
-  {
-    id: "update-user-in-db",
-    triggers: [{ event: "clerk/user.updated" }],
-  },
+  { id: "update-user-in-db", trigger: { event: "clerk/user.updated" } },
   async ({ event }) => {
     await connectDB();
-
     const { id, email_addresses, first_name, last_name, image_url } = event.data;
-
     const updatedUser = {
       email: email_addresses[0]?.email_address,
       name: `${first_name || ""} ${last_name || ""}`,
       profileImage: image_url,
     };
-
     await User.updateOne({ clerkId: id }, { $set: updatedUser });
-
-    await upsertStreamUser({
-      id: id.toString(),
-      name: updatedUser.name,
-      image: updatedUser.profileImage,
-    });
+    await upsertStreamUser({ id: id.toString(), name: updatedUser.name, image: updatedUser.profileImage });
   }
 );
 
