e8a64be9a30bf61419223dc557ede7baee6adb4a
shahateqx <shahat0209@gmail.com>
2026-05-03 13:59:54 +0600

    auth db setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,34 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
+import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
+
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
 
+import chatRoutes from "./routes/chatRoutes.js";
+import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 
 const __dirname = path.resolve();
 
 
-app.get("/selected", (req, res) => {
-  res.status(200).json({ message: "Hello, Sahat. You're selected!" });
+app.use(express.json());
+
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware());
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
 });
 
-app.get("/session", (req, res) => {
-  res.status(200).json({ message: "Wait for your session!" });
-});
 
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
@@ -30,7 +43,7 @@ const startServer = async () => {
     await connectDB();
     app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
   } catch (error) {
-    console.error("🛑 Error starting the server", error);
+    console.error("!Error starting the server", error);
   }
 };
 
