862f4b7b24ed642e13b42f85ecc82707ab5800c1
Shahat <shahat0209@gmail.com>
2026-05-03 14:14:36 +0600

    Clean up server.js by removing unused imports
    Removed unused middleware and routes from server.js.

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,13 @@ import express from "express";
 import path from "path";
 import cors from "cors";
 import { serve } from "inngest/express";
-import { clerkMiddleware } from "@clerk/express";
+
 
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { inngest, functions } from "./lib/inngest.js";
 
-import chatRoutes from "./routes/chatRoutes.js";
-import sessionRoutes from "./routes/sessionRoute.js";
+
 
 const app = express();
 
@@ -19,11 +18,10 @@ const __dirname = path.resolve();
 app.use(express.json());
 
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-app.use(clerkMiddleware());
+
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
-app.use("/api/chat", chatRoutes);
-app.use("/api/sessions", sessionRoutes);
+
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
